b25858844023be115f031f52856580ef482df8a4
quylong98x <93374976+nguyenviet212002@users.noreply.github.com>
2024-06-19 10:42:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,15 @@ include('header.php');
 require_once './api/dbcon.php';
 
 ?>
+<div class="part-1">
+    <?php
+    // Define the active page variable based on the current page
+    $active_page = basename($_SERVER['PHP_SELF'], ".php");
+    // Include the navbar.php file
+    include('side-bar.php');
+    ?>
 
+</div>
 
 <div class="part-2">
 
